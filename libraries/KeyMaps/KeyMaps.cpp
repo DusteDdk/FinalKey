2187e982c1fd6cc060fb55e3341a0aab85ab3cba
Jimmy Christensen <dusted@dusted.dk>
2014-09-12 13:04:18 +0300

    Consistent layout names and saved bytes.

--- libraries/KeyMaps/KeyMaps.cpp
+++ libraries/KeyMaps/KeyMaps.cpp
@@ -9,76 +9,78 @@
 
 void KeyMaps::setKbMap(uint8_t mapNum)
 {
+  ptxt("[Keyboard: ");
   switch(mapNum)
   {
 #ifdef KBMAP_USPC_PTR
     case KBMAP_USPC:
       Keyboard.begin( (uint8_t*)(KBMAP_USPC_PTR) );
-      ptxtln("[Keyboard: US-PC]");
+      ptxtln("USPC]");
     break;
 #endif
 
 #ifdef KBMAP_USMAC_PTR
     case KBMAP_USMAC:
       Keyboard.begin( (uint8_t*)(KBMAP_USMAC_PTR) );
-      ptxtln("[Keyboard: US-MAC]");
+      ptxtln("USMAC]");
     break;
 #endif
 
 #ifdef KBMAP_DKPC_PTR
     case KBMAP_DKPC:
       Keyboard.begin( (uint8_t*)(KBMAP_DKPC_PTR) );
-      ptxtln("[Keyboard: DK-PC]");
+      ptxtln("DKPC]");
     break;
 #endif
 #ifdef KBMAP_DKMAC_PTR
     case KBMAP_DKMAC:
       Keyboard.begin( (uint8_t*)(KBMAP_DKMAC_PTR) );
-      ptxtln("[Keyboard: DK-MAC]");
+      ptxtln("DKMAC]");
     break;
 #endif
 
 #ifdef KBMAP_FRPC_PTR
     case KBMAP_FRPC:
       Keyboard.begin( (uint8_t*)(KBMAP_FRPC_PTR) );
-      ptxtln("[Keyboard: FR-PC]");
+      ptxtln("FRPC]");
     break;
 #endif
 #ifdef KBMAP_FRMAC_PTR
     case KBMAP_FRMAC:
       Keyboard.begin( (uint8_t*)(KBMAP_FRMAC_PTR) );
-      ptxtln("[Keyboard: FR-MAC]");
+      ptxtln("FRMAC]");
     break;
 #endif
     
 #ifdef KBMAP_DEPC_PTR
     case KBMAP_DEPC:
       Keyboard.begin( (uint8_t*)(KBMAP_DEPC_PTR) );
-      ptxtln("[Keyboard: DE-PC]");
+      ptxtln("DEPC]");
     break;
 #endif
 #ifdef KBMAP_DEMAC_PTR
     case KBMAP_DEMAC:
       Keyboard.begin( (uint8_t*)(KBMAP_DEMAC_PTR) );
-      ptxtln("[Keyboard: DE-MAC]");
+      ptxtln("DEMAC]");
     break;
 #endif
 #ifdef KBMAP_SEPC_PTR
     case KBMAP_SEPC:
       Keyboard.begin( (uint8_t*)(KBMAP_SEPC_PTR) );
-      ptxtln("[Keyboard: SE-PC]");
+      ptxtln("SE-PC]");
     break;
 #endif
 #ifdef KBMAP_SEMAC_PTR
     case KBMAP_SEMAC:
       Keyboard.begin( (uint8_t*)(KBMAP_SEMAC_PTR) );
-      ptxtln("[Keyboard: SE-MAC]");
+      ptxtln("SEMAC]");
     break;
 #endif
     default:
-      ptxtln("[Keyboard: Unknown]");
+      ptxtln("ERROR]");
     break;
   }
+
 }
 
 KeyMaps kbmaps = KeyMaps();
